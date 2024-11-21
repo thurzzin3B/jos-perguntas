e60de9533548144a00667d06fdbf3dd93f4d8105
thurzzin3B <168560146+thurzzin3B@users.noreply.github.com>
2024-11-21 11:40:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    '12 ÷ 3 X 2 + 2 = ',
+    'A resposta é 10'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual time o Neymar torcia ?',
+    'Palmeiras'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual é o nome dos protagonistas do filme Senhor dos Aneis ?',
+    'Frodo e Sam'
+)
+
+criaCartao(
+    'Matematica',
+    'De 0 a 100 quntos numeros 3 tem ?',
+    'A resposta é 20'
+)
